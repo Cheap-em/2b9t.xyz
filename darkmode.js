17e188214f994132a712337a09f580eb59369c15
HowardPlayzOA <66976582+HPOA909@users.noreply.github.com>
2023-11-17 13:44:29 -0800

    darkmode js

--- darkmode.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('dark-mode-toggle');
-const container = document.getElementById('container');
-
-toggleButton.addEventListener('click', () => {
-  container.classList.toggle('dark');
-});
